cbbc7e6c295d0e5b553486ad9ab230cf68a0695f
Jody Phelan <jody.phelan@lshtm.ac.uk>
2020-11-13 10:06:23 +0000

    retrieve function

--- README.md
+++ README.md
@@ -12,11 +12,17 @@ cd tb-profiler-webdriver
 python setup.py install
 ```
 
-## Useage
+## Usage
 
 ```
 import import tbprofiler_webdriver
 
+# Create server object (default="https://tbdr.lshtm.ac.uk")
 server = tbprofiler_webdriver.tbprofiler_server()
+
+# Send fastq files to be processed
 run_id = server.send_fastq("sample_1.fastq.gz","sample_2.fastq.gz")
+
+# Retrieve results
+result = server.get_result(run_id)
 ```
--- tbprofiler_webdriver/__init__.py
+++ tbprofiler_webdriver/__init__.py
@@ -1,12 +1,13 @@
 import requests
+import json
 
 class tbprofiler_server:
     def __init__(self,host="http://localhost:5000"):
         self.host = host
     def send_fastq(self,fq1,fq2):
         files = {
-            "file1":open("/home/jody/tbprofiler_test_data/por5A1_1.fastq.gz","rb"),
-            "file2":open("/home/jody/tbprofiler_test_data/por5A1_2.fastq.gz","rb")
+            "file1":open(fq1,"rb"),
+            "file2":open(fq2,"rb")
         }
         data = {"platform":"illumina","single_sample_submit":""}
         print("%(host)s/upload" % vars(self),files,data)
@@ -14,5 +15,5 @@ class tbprofiler_server:
 
         return r.url.split("/")[-1]
 
-    def check_results(self,id):
-        return requests.get(self.host+"/results/"+id).content
+    def get_result(self,id):
+        return json.loads(requests.get(self.host+"/results/json/"+id).content)
